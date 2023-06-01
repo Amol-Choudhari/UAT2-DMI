bdf224a95081d4ebc1ea29654308e3650cef823e
unknown <bhadadelaxmi@gmail.com>
2023-06-01 15:48:51 +0530

    files updated

--- webroot/js/chemist/forward_applicationto_ral.js
+++ webroot/js/chemist/forward_applicationto_ral.js
@@ -11,8 +11,8 @@ $(document).ready(function(){
   })
   .on('changeDate', function (selected) {
     startDate = new Date(selected.date.valueOf());
-    startDate.setDate(startDate.getDate(new Date(selected.date.valueOf())));
-   
+    //startDate.setDate(startDate.getDate(new Date(selected.date.valueOf())));
+    startDate.setDate(startDate.getDate() + 1);    
     $('#sheduleTo').datepicker('setStartDate', startDate);
 });
 
@@ -23,13 +23,16 @@ $(document).ready(function(){
     setDate: new Date(),
     startDate:'+0d',
     format: 'dd/mm/yyyy',
+ 
     
   })
   .on('changeDate', function (selected) { 
     FromEndDate = new Date(selected.date.valueOf());
     FromEndDate.setDate(FromEndDate.getDate(new Date(selected.date.valueOf())));
+   
     $('#sheduleFrom').datepicker('setEndDate', FromEndDate);
 });
+
 
 
 
